0669ef43bbe94855a69572790c980bf831f1dbdf
KevinTouch <ktouch@uci.edu>
2020-02-01 21:45:09 -0800

    Added Files to Firebase and extra file

--- my-app/src/Content.jsx
+++ my-app/src/Content.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 
 import SlideShow from './SlideShow';
--- my-app/src/OngoingEventsForm.jsx
+++ my-app/src/OngoingEventsForm.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-
-
+import firebase from './firebase.js'; 
 
 class OngoingEventsForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+			currentItem: '',
+			username: ''
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this); 
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log("Button was clicked!");
+		const itemsRef = firebase.database().ref('items');
+  	const item = {
+	    title: this.state.currentItem,
+	    user: this.state.username
+	  }
+	  itemsRef.push(item);
+	  this.setState({
+	    currentItem: '',
+	    username: ''
+	  });
+	}
+
+
 	render() {
 		return ( 	
-			<div> Ongoing Events</div>
+			<div>
+				<section>
+				<form>
+					<input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+					<input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+					<button onClick={this.handleSubmit}>submit</button> 				
+					</form>
+				</section>
+			</div>
 		);
 	}
 }
