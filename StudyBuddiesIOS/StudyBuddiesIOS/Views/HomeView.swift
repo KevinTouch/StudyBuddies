3d35ce1af1ca6fb5afd62c4d566d55490a9551e5
Ugyen Dorji <tddorji101@gmail.com>
2020-02-02 03:01:08 -0800

    Created a view folder

--- StudyBuddiesIOS/StudyBuddiesIOS/Views/HomeView.swift
+++ StudyBuddiesIOS/StudyBuddiesIOS/Views/HomeView.swift
@@ -25,73 +25,22 @@ struct HomeView: View {
     @State private var selection = 0
     
     
-//    Dummy data will link to firebase database to pull info such as user's buddies, joined events
-    
-    
-    
-    let myevents: [Event] = [
-         .init(id: 0,eventname: "CS15 HW#2", details: "HW, ELH 100",course: "CS15",location: "ELH 100",date: "2/2/20",time:"12pm", statusImg: "currentevent"),
-         .init(id: 3,eventname: "Kevin's Tutoring Time", details: "Creating a dynamic list",course: "Kevin",location: "CSL 1",date: "2/2/20",time:"1pm",statusImg: "currentevent"),
-         .init(id: 5,eventname: "General Study Session", details: "General studying, starbucks at utc",course: "N/A",location: "Starbucks@UTC",date: "2/3/20",time:"2pm",statusImg: "futureevent"),
-         .init(id: 7,eventname: "Taran's Tutoring Time", details: "I'm smart come learn from me.",course: "Taran",location: "My House",date: "2/14/20",time:"9pm",statusImg: "futureevent")
-    ]
-    
-    let mybuddies: [Buddy] = [
-        .init(id: 0, uid:"123",name:"Taran",status:"Studying",location:"His house",imagename:"studying"),
-        .init(id: 1, uid:"124",name:"Kevin",status:"Studying",location:"CSL 1",imagename:"studying"),
-        .init(id: 2, uid:"125",name:"Peter",status:"Not Studying",location:"N/A",imagename:"study")
-    ]
-    
-    
-    
     var body: some View {
         TabView(selection: $selection) {
             
 //            Home View
-              NavigationView{
-                        VStack{
-                            
-                            ScrollView(.horizontal) {
-                                Text("My Events").font(.title)
-                                    .padding(.trailing, 440.0)
-                                HStack(spacing: 20) {
-                                               ForEach(myevents) {myevent in
-                                                    NavigationLink(destination: EventDetail()) {
-                                                        EventColumn(myevent: myevent)
-                                                    }.buttonStyle(PlainButtonStyle())
-                                                      
-                                               }
-                                }
-                                .padding(.horizontal)
-                            }
-                            
-                            Text("Buddies")
-                                .font(.title)
-                                .padding(.leading, -190.0)
-                            
-                            List(mybuddies){ buddy in
-                                   BuddyRow(buddy: buddy)
-                               }.navigationBarTitle(Text("Study Events")
-                                   .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                                   )
-                        }
-                        
-                       
-                        
-                }.tabItem { VStack(alignment: .center) {
-                Image("homes")
-                Text("Home")
-                } }.tag(1)
+            HomeTabView()
+                .tabItem { VStack(alignment: .center) {
+                    Image("homes")
+                    Text("Home")
+            } }.tag(1)
             
 //          Search/Explore View
             
-            NavigationView{
-                VStack{
-                    MapView()
-                }
-            }.tabItem { VStack(alignment: .center) {
-            Image("globe")
-            Text("Explore")
+            ExploreTabView()
+                .tabItem { VStack(alignment: .center) {
+                    Image("globe")
+                    Text("Explore")
             }  }.tag(2)
             
 //            Add Event
