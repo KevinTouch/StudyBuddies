dd0fb495cf688717740eb44e30f19e134a1dcb0a
KevinTouch <ktouch@uci.edu>
2020-02-02 02:24:23 -0800

    Adding handleChange to change state

--- my-app/src/CreateGroupForm.jsx
+++ my-app/src/CreateGroupForm.jsx
@@ -1,103 +1,123 @@
 import React, { Component } from 'react';
 import InputGroup, { InputGroupText } from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
-import * as NumericInput from "react-numeric-input";
+import firebase from './firebase.js'; 
 
 
 class CreateGroupForm extends Component {
-
 	constructor() {
 		super(); 
 		this.state = {
-			eventTitle: "",
-			major: "",
-			department: "", 
-			coursecode: "", 
-			coursenum: "" 
+			classType: "",
+			location: "",
+			participants: "", 
+			time: "", 
+			title: "" 
 	};
-
-	this.handleChange = this.handleChange.bind(this);
 }
 
+	handleChange = (e) => {
+		this.setState({ 
+			[e.target.name]: e.target.value 
+		});
+	};
 
-	handleChange(e) {
-		console.log(e.target.value)
+
+	handleSubmit = (e) => {		
+		e.preventDefault();
+		console.log("Button was clicked!");
+
+		let db = firebase.firestore();
+		db.collection("events").add({
+			title: this.state.currentItem,
+			user: this.state.username
+		})
+		.then(function(docRef) {
+				console.log("Document written with ID: ", docRef.id);
+		})
+		.catch(function(error) {
+				console.error("Error adding document: ", error);
+		});
+
 		this.setState({
-			[e.target.name]: e.target.value
+			currentItem: '',
+			username: ''
 		});
 	}
 
-	onHandleSubmit(e) {
-		e.preventDefault(); 
-		console.log("X");
-		/*const demop = {test: this.state.eventTitle};
-		console.log(demop); */
-	}
-
-	render() { 
-
+	render() {
+		const {classType, location, participants, time, title} = this.state;
 		return ( 
 			<div>
 				Create Events
-				<InputGroup className ="mb-3" name="eventTitle" onChange={this.handleChange} value={this.state.eventTitle}>
-					<InputGroup.Prepend> 
-						<InputGroup.Text id="basic-addon1" style={{paddingRight:"92px"}}>Event Title</InputGroup.Text>
-					</InputGroup.Prepend>
-					<FormControl
-						name="eventTitle" 
-						placeholder="Enter Event Titile"
-						aria-label="Event Title"
-						aria-describedby="basic-addon1"
-					/>
-				</InputGroup>
-				
-				<InputGroup className="mb-3">
+				<InputGroup className="mb-3" onChange={this.handleChange} value={classType} name="classType">
 			    <InputGroup.Prepend>
-			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"130px"}}>Time</InputGroup.Text>
-			    </InputGroup.Prepend>
-					<FormControl
-						name="eventTitle" 
-						placeholder="Time"
-						aria-label="Event Title"
-						aria-describedby="basic-addon1"
-					/>
-				</InputGroup>
-
-				<InputGroup className="mb-3" onChange={this.handleMajor}>
-			    <InputGroup.Prepend>
-			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"129px"}}>Place</InputGroup.Text>
-			    </InputGroup.Prepend>
-					<FormControl
-			      placeholder="Place"
-			      aria-label="Place"
-			      aria-describedby="basic-addon1"
-			    />
-				</InputGroup>
-
-
-				<InputGroup className="mb-3">
-			    <InputGroup.Prepend>
-			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"115px"}}>Subject</InputGroup.Text>
+			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"60px"}}>Class</InputGroup.Text>
 			    </InputGroup.Prepend>
 			    <FormControl
-			      placeholder="Subject"
-			      aria-label="Subject"
+						name="classType"
+			      placeholder="Class Name"
+			      aria-label="classType"
 			      aria-describedby="basic-addon1"
 			    />
 				</InputGroup>
-				
-				<InputGroup className="mb-3">
+
+				<InputGroup className="mb-3" onChange={this.handleChange} value={location} name="location">
 			    <InputGroup.Prepend>
-			      <InputGroup.Text>Number People Going</InputGroup.Text>
+			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"35px"}}>Location</InputGroup.Text>
+			    </InputGroup.Prepend>
+					<FormControl
+						name="location"
+			      placeholder="Location"
+			      aria-label="location"
+			      aria-describedby="basic-addon1"
+			    />
+				</InputGroup>
+
+				<InputGroup className="mb-3" onChange={this.handleChange} value={participants} name="participants">
+			    <InputGroup.Prepend>
+			      <InputGroup.Text>Participants</InputGroup.Text>
 			    </InputGroup.Prepend>
 				  <FormControl
-			      placeholder="Number People Going"
-			      aria-label="Number People Going"
+						name="participants"
+			      placeholder="# of Participants"
+			      aria-label="participants"
 			      aria-describedby="basic-addon1"
 			    />
 				</InputGroup>
-				</div>
+
+
+				<InputGroup className="mb-3" onChange={this.handleChange} value={time} name="time">
+			    <InputGroup.Prepend>
+			      <InputGroup.Text id="basic-addon1" style={{paddingRight:"60px"}}>Time</InputGroup.Text>
+			    </InputGroup.Prepend>
+					<FormControl
+						name="time" 
+						placeholder="Time"
+						aria-label="Time"
+						aria-describedby="basic-addon1"
+					/>
+				</InputGroup>
+
+				<InputGroup className ="mb-3"onChange={this.handleChange} value={title} name="title">
+					<InputGroup.Prepend> 
+						<InputGroup.Text id="basic-addon1" style={{paddingRight:"65px"}}>Title</InputGroup.Text>
+					</InputGroup.Prepend>
+					<FormControl
+						name="title" 
+						placeholder="Enter Event Titile"
+						aria-label="Title"
+						aria-describedby="basic-addon1"
+					/>
+				</InputGroup>
 				
+
+
+
+
+
+
+				</div>
 			);
 		}
 	}
